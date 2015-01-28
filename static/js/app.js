d22b144c1cd7eaade02b8e59175602c270ebe5f4
gkamradt <gkamradt@gmail.com>
2015-01-27 21:51:29 -0500

    Initial commit

---
+++ static/js/app.js
@@ -0,0 +1,25 @@
+var app = angular.module("app", []);
+
+app.controller("AppCtrl", function($http){
+    var app = this;
+
+    $http.get("/api/pin").success(function (data){
+        app.pins = data.objects;
+    })
+
+    app.addPin = function (){
+        $http.post("/api/pin",{"title":"new", "image":"http://placekitten.com/200/200/?image=" + app.pins.length})
+            .success(function (data){
+                app.pins.push(data);
+            })
+    }
+
+    app.deletePin = function (pin){
+        $http.delete("/api/pin/" + pin.id).success(function (response){
+            app.pins.splice(app.pins.indexOf(pin), 1)
+        })
+    }
+    app.updatePin = function (pin) {
+        $http.put("/api/pin/" + pin.id, pin);
+    }
+})
